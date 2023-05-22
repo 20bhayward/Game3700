0d32ecd7fa366a415b69db37502d196e9ce9a433
Novel <50412055+20bhayward@users.noreply.github.com>
2023-05-22 19:53:22 -0400

    Finish Cave Island

--- Assets/Blocks/Secrets/Bloodvine/BloodvineController.cs
+++ Assets/Blocks/Secrets/Bloodvine/BloodvineController.cs
@@ -1,90 +1,51 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BloodvineController : MonoBehaviour
 {
-    public float frameDuration = 1f;  // duration of each frame in seconds
-    public float loopDelay = 1f;  // delay between loops in seconds
-    public bool loop = true;  // whether the animation should loop
-    public BoxCollider2D collider;
-
-    private GameObject[] frames;
-    private int currentFrame = 0;
-
-    private void Start()
-    {
-        // initialize frames array with child GameObjects
-        frames = new GameObject[transform.childCount];
-        for (int i = 0; i < frames.Length; i++)
-        {
-            frames[i] = transform.GetChild(i).gameObject;
-        }
-
-        // start the animation
-        StartCoroutine(Animate());
-    }
-
-
-    private IEnumerator Animate()
-    {
-        while (true)
-        {
-            // deactivate current frame
-            if (currentFrame > 0)
-            {
-                frames[currentFrame - 1].SetActive(false);
-            }
-
-            // activate next frame
-            frames[currentFrame].SetActive(true);
-
-            // update collider size
-            collider.size = frames[currentFrame].GetComponent<BoxCollider2D>().size;
-
-            // wait for the next frame
-            yield return new WaitForSeconds(frameDuration);
-
-            // increment current frame
-            currentFrame++;
-
-            // if reached the end of the animation
-            if (currentFrame == frames.Length)
-            {
-                // deactivate last frame
-                frames[currentFrame - 1].SetActive(false);
-
-                // reset current frame
-                currentFrame = 0;
-
-                if (loop)
-                {
-                    // activate first frame
-                    frames[0].SetActive(true);
-
-                    // wait for the loop delay
-                    yield return new WaitForSeconds(loopDelay);
-                }
-                else
-                {
-                    // stop the animation
-                    frames[0].SetActive(true);  // activate first frame
-                    yield break;
-                }
-            }
-        }
-    }
-
-
-
+    public List<GameObject> frames;
+    public float timeBetweenFrames = 0.1f;
+    public bool loop = false;
+    public float timeBetweenLoops = 1.0f;
+    private bool isAnimating = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !isAnimating)
         {
-            // restart the animation
-            StopCoroutine(Animate());
-            currentFrame = 0;
             StartCoroutine(Animate());
         }
+    }
+
+    private IEnumerator Animate()
+    {
+        isAnimating = true;
+
+        while (true)
+        {
+            for (int i = 0; i < frames.Count; i++)
+            {
+                if (i > 0)
+                {
+                    frames[i - 1].SetActive(false);
+                }
+
+                frames[i].SetActive(true);
+                yield return new WaitForSeconds(timeBetweenFrames);
+            }
+
+            if (!loop)
+            {
+                isAnimating = false;
+                break;
+            }
+
+            frames[frames.Count - 1].SetActive(false);
+            yield return new WaitForSeconds(timeBetweenLoops);
+        }
+
+        frames[0].SetActive(true);
+        isAnimating = false;
     }
 }
